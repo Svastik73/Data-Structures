969dd28772254e65ffa1276dee491c404c8f4a43
volvarz73 <75065361+Svastik73@users.noreply.github.com>
2022-12-09 19:14:56 +0530

    Update activity_select.c

--- Greedy/activity_select.c
+++ Greedy/activity_select.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 typedef struct activity{
     int start;
     int end;
 }activity;
 void ssort(activity arr[],int n){
     for(int i=0;i<n-1;i++){
-        int maz=arr[i].end;
+        int maz=i;
         for(int j=i+1;j<n;j++){
-            if(arr[j].end<maz) maz=arr[j].end;
+            if(arr[j].end<arr[maz].end) maz=j;
         }
-
     int t1=arr[i].start; int t2=arr[i].end;
     arr[i].start=arr[maz].start; arr[i].end=arr[maz].end;
     arr[maz].start=t1; arr[maz].end=t2;
@@ -20,7 +18,7 @@ void ssort(activity arr[],int n){
 int activity_select(activity arr[],int n){
        ssort(arr,n); int count=1;
       int max=arr[0].end;
-       printf("Activity 1 selected and time of end is %d: ",arr[0].end);
+       printf("\nActivity 1 selected and time of end is %d: ",arr[0].end);
      for(int i=1;i<n;i++){
         if(arr[i].start>=max){
             printf("\nActivity %d selected and time of end is %d: ",i+1,arr[i].end);
@@ -38,14 +36,11 @@ int main(){
     printf("\nEnter the details of activity\n ");
     for(int i=0;i<n;i++){ printf("\nenter start:");  scanf("%d",&arr[i].start);
     printf("\nenter end: "); scanf("%d",&arr[i].end);
-
     }
         fflush(stdin);
     for(int i=0;i<n;i++){ printf("%d ",arr[i].start);
     printf("%d ",arr[i].end);
-
     }
     int t=activity_select(arr,n);
     printf("\n total events scheduled are : %d",t);
-
 }
