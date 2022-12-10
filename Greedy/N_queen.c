89091eff03c9ceb8168dbff59d2da8c5f47d72dd
volvarz73 <75065361+Svastik73@users.noreply.github.com>
2022-12-10 23:27:31 +0530

    Create N_queen.c

---
+++ Greedy/N_queen.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+int x[50];  // x[i] will store column no. of queen number i!
+int place(int k,int xk){  
+    int j;
+    for(j=1;j<=k;j++){
+        if(x[j]==xk || abs(x[j]-xk)==abs(j-k)){
+            return 0;
+            //  xk is position not acceptable for queen!
+        }
+    }
+    return 1;
+}
+void display(int n){
+    int i;
+    for(i=1;i<=n;i++){
+        printf("\nPosition of %d queen is:%d ",i,x[i]);
+    }
+}
+void Nqueen(int k,int n){  // k is row
+    int column;  // k is the number of queen entered !
+    for(column=1; column<=n; column++){   // this ensure automatic backtrack!!
+        if(place(k,column)==1){
+            x[k]=column;
+            if(k==n){  // means last queen so no more recursion
+                display(n);
+                exit(0);
+            }
+            else Nqueen(k+1,n);
+        }
+    }
+}
+int main(){
+  int n; printf("Enter value of sides in chessboard: \n"); scanf("%d",&n);
+  Nqueen(1,n);  // pass k==1 here to start from entering 1 queen only!
+}
