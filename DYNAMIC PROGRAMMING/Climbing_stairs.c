67a2dbeec6fb3fdac5b887851725b30a41607bb5
volvarz73 <75065361+Svastik73@users.noreply.github.com>
2023-07-24 12:37:54 +0530

    Update Climbing_stairs.c

--- DYNAMIC PROGRAMMING/Climbing_stairs.c
+++ DYNAMIC PROGRAMMING/Climbing_stairs.c
@@ -1,1 +1,32 @@
-
+/*
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2)
+         return n;
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;  
+        dp[2]=2;
+        for(int i=3;i<=n;i++)
+         dp[i]=dp[i-1]+dp[i-2];
+        // check for n-1 and n-2 step as till it we can add one step more!
+        return dp[n];
+    }
+};
+  */
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2)
+         return n;
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;  
+        dp[2]=2;
+        for(int i=3;i<=n;i++)
+         dp[i]=dp[i-1]+dp[i-2];
+        // check for n-1 and n-2 step as till it we can add one step more!
+        return dp[n];
+    }
+};
