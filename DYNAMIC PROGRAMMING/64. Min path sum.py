6b04a07a9065df71e04d3f6dcf7b25eea44fd8d0
volvarz73 <75065361+Svastik73@users.noreply.github.com>
2023-08-05 01:03:00 +0530

    Create 64. Min path sum.py

---
+++ DYNAMIC PROGRAMMING/64. Min path sum.py
@@ -0,0 +1,28 @@
+'''
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+'''
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        n = len(grid)
+        m = len(grid[0])
+        ls = []
+        for i in range(len(grid)):
+            l=[]
+            for j in range(len(grid[i])):
+                l.append(0)
+            ls.append(l)
+        ls[0][0] = grid[0][0]
+        for i in range(n):
+            for j in range(m):
+                if i != 0 or j != 0:
+                    r = 201 if i == 0 else ls[i-1][j]
+                    c = 201 if j == 0 else ls[i][j-1]
+                    ls[i][j] = grid[i][j] + min(r, c)
+        return ls[n-1][m-1]
